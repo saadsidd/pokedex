dc21fa752a14470ec3ff9edac44467e2b975b90b
Saad Siddiq <siddiq.saad@gmail.com>
2023-02-09 19:18:30 -0700

    added header and a pokedex image + title

--- client/src/components/PokemonTypeSelect.js
+++ client/src/components/PokemonTypeSelect.js
@@ -1,29 +1,36 @@
 import { useState, useEffect } from "react";
-import { TypeSelect } from "./styles/TypeSelect.styled";
+import { TypeSelectContainer, PokedexImage, TypeSelect } from "./styles/TypeSelect.styled";
 
 export default function PokemonTypeSelect({ handleChoose }) {
 
   const [types, setTypes] = useState(null);
 
-  const fetchTypes = async () => {
-    const data = await fetch('/types');
-    const typesJSON = await data.json();
-    const typesArray = typesJSON.map(type => type.english);
-    setTypes(typesArray);
-  }
+  useEffect(() => {
+
+    const fetchTypes = async () => {
+      const data = await fetch('/types');
+      const typesJSON = await data.json();
+      const typesArray = typesJSON.map(type => type.english);
+      setTypes(typesArray);
+    }
+
+    fetchTypes();
+    
+  }, []);
 
   const createOptions = (types) => {
     return types.map((type, index) => <option key={index} value={type}>{type}</option>);
   }
 
-  useEffect(() => {
-    fetchTypes();
-  }, []);
-
   return (
-    <TypeSelect defaultValue='placeholder' onChange={event => handleChoose(event.target.value)}>
-      <option value='placeholder' disabled>Select a Pokémon type</option>
-      {types && createOptions(types)}
-    </TypeSelect>
+    <TypeSelectContainer>
+      <PokedexImage>
+        <img src='./pokedex.png' alt='pokedex' />
+      </PokedexImage>
+      <TypeSelect defaultValue='placeholder' onChange={event => handleChoose(event.target.value)}>
+        <option value='placeholder' disabled>Select a Pokémon type</option>
+        {types && createOptions(types)}
+      </TypeSelect>
+    </TypeSelectContainer>
   )
 }
--- client/src/components/styles/TypeSelect.styled.js
+++ client/src/components/styles/TypeSelect.styled.js
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 
+export const TypeSelectContainer = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 10px 0px;
+  background-color: #f1dddd;
+`;
+
+export const PokedexImage = styled.div`
+  & > img {
+    width: 200px;
+  }
+`;
+
 export const TypeSelect = styled.select`
-  width: 400px;
+  width: 300px;
   height: 50px;
   padding-left: 20px;
-  margin-top: 25px;
   font-size: 20px;
 `;
