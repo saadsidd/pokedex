192dc9031c71b06acdb079966a88c9200731eb5f
Saad Siddiq <siddiq.saad@gmail.com>
2023-02-26 01:52:41 -0700

    creating pokedexByType obj on server start. better performance

--- server/server.js
+++ server/server.js
@@ -2,38 +2,45 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));               // Logs all http requests to console to help development
+app.use(express.static('./public'));  // Making thumbnails folder static
 const PORT = 8080;
+
 const types = require('./data/types.json');
 const pokedex = require('./data/pokedex.json');
+const pokedexByType = {};
 
-// Logs all http requests to console to help development
-app.use(morgan('dev'));
-// Making thumbnails folder static
-app.use(express.static('./public'));
-
-
-// Filter pokedex.json by matching type and return sliced 10 results
-const getPokemonByTypeAndTotalPages = (type, page) => {
-  const pageNum = parseInt(page);
-  const start = (pageNum - 1) * 10;
-  const end = pageNum * 10;
-
-  const filtered = pokedex.filter(pokemon => pokemon.type.includes(type));
-  const tenPokemon = filtered.slice(start, end);
-  const totalPages = Math.ceil(filtered.length / 10);
-  return {tenPokemon, totalPages};
-}
+// Populate pokedexByType object on server start for easier access to data and more performant
+pokedex.forEach(pokemon => {
+  pokemon.type.forEach(type => {
+    if (pokedexByType[type] === undefined) pokedexByType[type] = [];
+    pokedexByType[type].push(pokemon);
+  });
+});
 
 // Sends all of types.json to populate drop-down menu
 app.get('/types', (req, res) => {
   res.send(types);
 });
 
-// Sends filtered array of pokemon objects based on given type and page number
+// Sends all pokemon of a given type
+app.get('/types/:type', (req, res) => {
+  const { type } = req.params;
+  res.send(pokedexByType[type]);
+});
+
+// Sends 10 pokemon based on given type/page number, and total pages of that type
 app.get('/types/:type/:page', (req, res) => {
   const { type, page } = req.params;
-  const results = getPokemonByTypeAndTotalPages(type, page);
-  res.send(results);
+
+  const pageNum = parseInt(page);
+  const start = (pageNum - 1) * 10;
+  const end = pageNum * 10;
+
+  const tenPokemon = pokedexByType[type].slice(start, end);
+  const totalPages = Math.ceil(pokedexByType[type].length / 10);
+
+  res.send({ tenPokemon, totalPages });
 });
 
 app.listen(PORT, () => console.log(`Server started. Listening on port ${PORT}`));
